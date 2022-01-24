2f7b0d4b2382ea015a7a2b01d7c15b2516bef8ac
Jonatan Norbye <jonatan@norbye.com>
2022-01-24 18:04:26 +0100

    Implement import ntf page in the frontend

---
+++ src/pages/import/Import.tsx
@@ -0,0 +1,94 @@
+import { gql, useMutation } from "@apollo/client";
+import { useState } from "react";
+import { CollectionItem } from "../../features/collections/CollectionItem";
+import styles from "./Import.module.css";
+
+const IMPORT_NFT = gql`
+  mutation ImportNft($input: ImportNftInput!) {
+    importNft(input: $input) {
+      id
+      image_url
+      name
+      creator {
+        user {
+          username
+        }
+      }
+    }
+  }
+`;
+
+export function Import() {
+  const [importNft, { data, loading, error }] = useMutation(IMPORT_NFT);
+
+  const [insertNft, setInsertNft] = useState(true);
+  let address: HTMLInputElement | null;
+  let token_id: HTMLInputElement | null;
+
+  if (loading) return <p>Loading...</p>;
+  if (error && !insertNft)
+    return (
+      <div className={styles.error}>
+        <h2>Error</h2>
+        <p>Failed to import NFT</p>
+        <button
+          onClick={() => {
+            setInsertNft(true);
+          }}
+        >
+          Add another NFT
+        </button>
+      </div>
+    );
+
+  if (data && !insertNft) {
+    return (
+      <main className={styles.import}>
+        <h2>NFT Imported successfully</h2>
+        <CollectionItem {...data.importNft} />
+        <button
+          onClick={() => {
+            setInsertNft(true);
+          }}
+        >
+          Add another NFT
+        </button>
+      </main>
+    );
+  }
+
+  return (
+    <main className={styles.import}>
+      <h2>Import NFT</h2>
+      <p>Address of the contract for the NFT</p>
+      <input
+        ref={(node) => {
+          address = node;
+        }}
+      />
+      <p>Token ID for the NFT</p>
+      <input
+        ref={(node) => {
+          token_id = node;
+        }}
+      />
+      <button
+        onClick={() => {
+          importNft({
+            variables: {
+              input: {
+                asset_contract_address: address?.value,
+                token_id: token_id?.value,
+              },
+            },
+          });
+          setInsertNft(false);
+          address = null;
+          token_id = null;
+        }}
+      >
+        Import
+      </button>
+    </main>
+  );
+}
