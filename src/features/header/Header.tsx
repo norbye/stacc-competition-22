2156f98ff47b10f69307189e7a34d5ff2f9d4af8
Jonatan Norbye <jonatan@norbye.com>
2022-01-24 15:12:31 +0100

    Setup react navigation and header for app

---
+++ src/features/header/Header.tsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import styles from "./Header.module.css";
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <Link to={"/"}>
+        <div className={styles.logoWrapper}>
+          <span>]NFTBase[</span>
+        </div>
+      </Link>
+      <span>
+        <Link to={"/"}>Gallery</Link>
+      </span>
+      <span>
+        <Link to={"/import"}>Import NFT</Link>
+      </span>
+      <span>
+        <Link to={"/create"}>Create NFT</Link>
+      </span>
+    </header>
+  );
+}
